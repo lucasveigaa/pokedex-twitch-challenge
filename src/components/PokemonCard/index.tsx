4765130d67db71c9931b9a2cab84e0572e8c1e35
lucasveigaa <lucasveigagonzaga@yahoo.com.br>
2023-01-28 08:52:05 -0300

    handle loading, refetching and error states

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -2,10 +2,35 @@ import { useGetPokemon } from '@/queries/useGetPokemon'
 import { TPokemonListEndpointResult } from '../../interfaces'
 
 export const PokemonCard = (pokemon: TPokemonListEndpointResult) => {
-    const { data } = useGetPokemon(pokemon.url)
+    const { data, isFetching, isError, refetch } = useGetPokemon(pokemon.url)
+
+    if (isError)
+        return (
+            <div className="w-[320px] min-h-[124px] bg-sun-400 shadow rounded-lg text-white text-xs p-4 flex flex-col gap-4 text-center pt-7">
+                Erro ao carregar o pokémon
+                <button
+                    className="border border-sun-50 w-fit px-2 py-1 mx-auto rounded-md"
+                    onClick={() => refetch()}
+                >
+                    Tentar novamente
+                </button>
+            </div>
+        )
+
+    if (isFetching)
+        return (
+            <div className="w-[320px] min-h-[124px] bg-sun-400 shadow rounded-lg pt-5">
+                <div className="animate-pulse flex flex-col gap-4">
+                    <div className="mx-auto mt-2 rounded-full bg-slate-200 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1 px-14">
+                        <div className="h-2 bg-slate-200 rounded"></div>
+                    </div>
+                </div>
+            </div>
+        )
 
     return (
-        <div className="flex flex-col items-center justify-center min-w-[320px] py-4 rounded-lg bg-sun-300">
+        <div className="flex flex-col items-center justify-center min-w-[320px] py-4 rounded-lg bg-sun-400">
             <img
                 loading="lazy"
                 className="w-16 h-16"
@@ -16,7 +41,7 @@ export const PokemonCard = (pokemon: TPokemonListEndpointResult) => {
                     data?.sprites?.other?.['official-artwork']?.front_default
                 }
             />
-            <span className="uppercase mt-3 text-xs text-white font-pressStart font-normal">
+            <span className="uppercase mt-3 text-xs text-white font-normal">
                 {pokemon.name}
             </span>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,38 @@
-import { PokemonCard } from '@/components/PokemonCard'
+import { PokemonCard, LoadingStatus } from '@/components'
 import { useGetPokemonList } from '@/queries/useGetPokemonList'
 
 export default function Home() {
-    const { data, fetchNextPage, hasNextPage, refetch } = useGetPokemonList()
+    const { data, fetchNextPage, hasNextPage, refetch, isFetching } =
+        useGetPokemonList()
 
     return (
-        <div>
-            <div className="flex items-center justify-center py-10 gap-4 flex-wrap bg-sun-50">
+        <div className="bg-sun-50 min-h-screen">
+            <h1 className="flex justify-center pt-14 text-4xl text-sun-400 animate-bounce transition-all">
+                Lista de Pokémons
+            </h1>
+
+            <div className="flex items-center justify-center py-10 gap-4 flex-wrap ">
                 {data?.pages.map((page) =>
                     page.results.map((pokemon) => <PokemonCard {...pokemon} />)
                 )}
             </div>
-            <button onClick={() => (hasNextPage ? fetchNextPage() : refetch())}>
-                Next
-            </button>
+
+            <div className="flex justify-center pb-4">
+                {isFetching ? (
+                    <div className="flex gap-2">
+                        <LoadingStatus /> Carregando...
+                    </div>
+                ) : (
+                    <button
+                        className="flex gap-2"
+                        onClick={() =>
+                            hasNextPage ? fetchNextPage() : refetch()
+                        }
+                    >
+                        Carregar mais...
+                    </button>
+                )}
+            </div>
         </div>
     )
 }
