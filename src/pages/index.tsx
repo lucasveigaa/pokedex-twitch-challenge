54bfc23638ae2833dda84d9b51a760fc6e8b83e7
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2023-02-04 12:25:32 -0300

    feat: add infinity list with scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
 
 import { PokemonCard, LoadingStatus, ThemeToggler } from '@/components'
 import { useDebounce } from '@/hooks'
@@ -11,8 +12,7 @@ export default function Home() {
         searchPokemon.toLocaleLowerCase()
     )
 
-    const { data, fetchNextPage, hasNextPage, refetch, isFetching } =
-        useGetPokemonList()
+    const { data, fetchNextPage, hasNextPage } = useGetPokemonList()
 
     return (
         <div className="transition-colors bg-fixed animate-infinityParallax pattern-cross pattern-white dark:pattern-white pattern-bg-sun-50 dark:pattern-sun-300 dark:pattern-bg-sun-500 pattern-size-6 pattern-opacity-100 px-4 sm:px-8 lg:px-36 min-h-screen">
@@ -39,33 +39,23 @@ export default function Home() {
                 <PokemonCard url={`pokemon/${searchPokemonDebounced}`} />
             </div>
 
-            <div className="flex items-center justify-center sm:justify-between py-10 gap-4 flex-wrap ">
-                {data?.pages.map((page) =>
-                    page.results.map((pokemon) => (
-                        <PokemonCard
-                            url={pokemon.url}
-                            pokemonName={pokemon.name}
-                        />
-                    ))
-                )}
-            </div>
-
-            <div className="flex justify-center pb-4">
-                {isFetching ? (
-                    <div className="flex gap-2">
-                        <LoadingStatus /> Carregando...
-                    </div>
-                ) : (
-                    <button
-                        className="flex gap-2"
-                        onClick={() =>
-                            hasNextPage ? fetchNextPage() : refetch()
-                        }
-                    >
-                        Carregar mais...
-                    </button>
-                )}
-            </div>
+            <InfiniteScroll
+                loadMore={() => fetchNextPage()}
+                hasMore={hasNextPage}
+                loader={<LoadingStatus key={0} />}
+            >
+                <div className="flex items-center justify-center sm:justify-between py-10 gap-4 flex-wrap">
+                    {data?.pages.map((page) =>
+                        page.results.map((pokemon) => (
+                            <PokemonCard
+                                key={pokemon.name}
+                                url={pokemon.url}
+                                pokemonName={pokemon.name}
+                            />
+                        ))
+                    )}
+                </div>
+            </InfiniteScroll>
         </div>
     )
 }
