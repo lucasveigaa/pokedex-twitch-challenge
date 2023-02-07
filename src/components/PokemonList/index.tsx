9a429f09ac07f52c6fff83f34ca09452fc0c7674
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2023-02-07 10:08:44 -0300

    refactor: change useMemo to memo hook on pokemon list

--- src/components/PokemonList/index.tsx
+++ src/components/PokemonList/index.tsx
@@ -1,33 +1,29 @@
 import InfiniteScroll from 'react-infinite-scroller'
-import { useMemo } from 'react'
+import { memo } from 'react'
 
 import { useGetPokemonList } from '@/queries'
 import { LoadingStatus, PokemonCard } from '..'
 
-export const PokemonList = () => {
+export const PokemonList = memo(() => {
     const { data, fetchNextPage, hasNextPage } = useGetPokemonList()
 
-    const memoData = useMemo(() => {
-        return (
-            <InfiniteScroll
-                loadMore={() => fetchNextPage()}
-                hasMore={hasNextPage}
-                loader={<LoadingStatus key={0} />}
-            >
-                <div className="flex items-center justify-center sm:justify-between py-10 gap-4 flex-wrap">
-                    {data?.pages.map((page) =>
-                        page.results.map((pokemon) => (
-                            <PokemonCard
-                                key={pokemon.name}
-                                url={pokemon.url}
-                                pokemonName={pokemon.name}
-                            />
-                        ))
-                    )}
-                </div>
-            </InfiniteScroll>
-        )
-    }, [data])
-
-    return <div>{memoData}</div>
-}
+    return (
+        <InfiniteScroll
+            loadMore={() => fetchNextPage()}
+            hasMore={hasNextPage}
+            loader={<LoadingStatus key={0} />}
+        >
+            <div className="flex items-center justify-center sm:justify-between py-10 gap-4 flex-wrap">
+                {data?.pages.map((page) =>
+                    page.results.map((pokemon) => (
+                        <PokemonCard
+                            key={pokemon.name}
+                            url={pokemon.url}
+                            pokemonName={pokemon.name}
+                        />
+                    ))
+                )}
+            </div>
+        </InfiniteScroll>
+    )
+})
